757789606ce494a1e5ab32fa782a2b47512c3315
ambro <mnflambr@gmail.com>
2021-10-10 22:01:30 -0500

    carga inicial

---
+++ src/main/java/com/example/demodeployoc/DemoDeployOcApplication.java
@@ -0,0 +1,16 @@
+package com.example.demodeployoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Generated;
+
+@SpringBootApplication
+public class DemoDeployOcApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeployOcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demodeployoc/DemoDeployOcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodeployoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeployOcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
